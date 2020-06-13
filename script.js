b12884d43c4abe7e7ff657854ee36a554ef9f17a
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-06-13 12:26:19 +0200

    Get DOM elements and add data array

---
+++ script.js
@@ -0,0 +1,57 @@
+const main = document.querySelector('main');
+const selectVoice = document.getElementById('select-voice');
+const text = document.getElementById('text');
+const toggleBtn = document.getElementById('btn-toggle');
+const readBtn = document.getElementById('btn-read');
+const closeBtn = document.getElementById('close');
+
+// Create data
+const data = [{
+        image: './Image/thirsty.jpg',
+        text: "I'm thirsty"
+    },
+    {
+        image: './Image/hungry.jpg',
+        text: "I'm hungry"
+    },
+    {
+        image: './Image/tired.jpg',
+        text: "I'm tired"
+    },
+    {
+        image: './Image/hurt.jpg',
+        text: "I'm hurt"
+    },
+    {
+        image: './Image/happy.jpg',
+        text: "I'm happy"
+    },
+    {
+        image: './Image/angry.jpg',
+        text: "I'm angry"
+    },
+    {
+        image: './Image/sad.jpg',
+        text: "I'm sad"
+    },
+    {
+        image: './Image/scared.jpg',
+        text: "I'm scared"
+    },
+    {
+        image: './Image/outside.jpg',
+        text: "I want to go outside"
+    },
+    {
+        image: './Image/home.jpg',
+        text: "I want to go home"
+    },
+    {
+        image: './Image/school.jpg',
+        text: "I want to go to school"
+    },
+    {
+        image: './Image/grandma.jpg',
+        text: "I want to go to grandma"
+    },
+]
